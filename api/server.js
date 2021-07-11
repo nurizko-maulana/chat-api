9baaaf3747906c8ad516eaa96f70d9f879a6d791
Nurizko Maulana <nurizko.maulana@gmail.com>
2021-07-12 00:42:41 +0700

    Add user controller and model

--- api/server.js
+++ api/server.js
@@ -1,14 +1,17 @@
 const port = process.env.PORT || 3002
 const express = require('express')
 const messageRouter = require('./controllers/messages_controller')
+const userRouter = require('./controllers/users_controller')
 
 const app = express()
 
 app.use(express.json())
 
+
 app.get('/', (req, res, next) => res.send(`Server is running on port ${port}`));
 
 app.use('/message', messageRouter)
+app.use('/user', userRouter)
 
 
-app.listen(port, () => { console.log(`Server is running on port ${port}`) })
+app.listen(port, () => { console.log(`Server is running on port http://localhost:${port}`) })
